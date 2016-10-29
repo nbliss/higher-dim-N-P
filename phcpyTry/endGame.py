a12e2f2aadca9c4823e4cd74db4fbd585ca19f7e
nbliss <nbliss2@uic.edu>
2016-10-29 17:10:40 -0500

    Added ability to sweep along plane givven by normal ray, instead of just the x0=c plane

--- phcpyTry/endGame.py
+++ phcpyTry/endGame.py
@@ -1,4 +1,4 @@
-def setupSystem(polys,expandVar = 'x1'):
+def setupSystem(polys,v,expandVar = 'x1'):
     """
     Given a space curve system polys, returns
     f,g,gsols where gsols are solutions of g,
@@ -6,18 +6,28 @@ def setupSystem(polys,expandVar = 'x1'):
     """
     from phcpy.solver import solve
     randy = randUnitCircle()
-    startSyst = polys+[expandVar+ '-' + str(randy) + ';'] 
+    variables = ['x'+str(i) for i in xrange(1,4)]
+    #startSyst = polys+[expandVar+ '-' + str(randy) + ';'] 
+    extraPoly = [str(v[i])+'*'+variables[i] for i in xrange(3)]
+    extraPoly = ' + '.join(extraPoly) 
+    targetPoly = [extraPoly + ';']
+    extraPoly += ' + 1;'
+    extraPoly = [extraPoly]
+    print extraPoly
+    startSyst = polys+extraPoly
     startSols = solve(startSyst,silent=True)
-    return (polys+[expandVar+';'],startSyst,startSols)
+    #return (polys+[expandVar+';'],startSyst,startSols)
+    print (polys+targetPoly,startSyst)
+    return (polys+targetPoly,startSyst,startSols)
 
-def performEndGame(polys,expandVar = 'x1'):
+def performEndGame(polys,v,expandVar = 'x1'):
     """
     Performs a polyhedral end game on the system in polys,
     which should be n-1 equations in n unknowns, with expandVar
     the expanding variable. Returns the winding numbers, coordinates
-    of the direction vectors, and the errrors.
+    of the direction vectors, and the errors.
     """
-    f,g,gsols = setupSystem(polys,expandVar)
+    f,g,gsols = setupSystem(polys,v,expandVar)
     #from phcpy.solver import mixed_volume as mv
     #from phcpy.solver import random_coefficient_system as rcs
     from phcpy.tuning import order_endgame_extrapolator_set as setOrder
@@ -31,6 +41,10 @@ def performEndGame(polys,expandVar = 'x1'):
     (w, d, e) = retrieve(len(sols), len(f))
     #return (w,d,e)
     print e
+    #d = [roundVect(v) for v in d]
+    #n = len(d[0])/2
+    #return [[v[i]+v[i+n] for i in xrange(n)] for v in d]
+    #return d
     return [roundVect(v) for v in d]
 
 def randUnitCircle():
@@ -56,7 +70,9 @@ def roundVect(v):
     return [roundToInt(i) for i in v]
 
 
-#f = ['x1^2 - x1 + x2 + x3;', 'x2^2 + x1 + x2 + x3;']
+#l = ['x1^2 - x1 + x2 + x3;', 'x2^2 + x1 + x2 + x3;']
+l = ['x2^2 - x1 + x2 + x3;', 'x3^2 + x1 + x2 + x3;']
 #print setupSystem(f)
-l = ['x1^2 - x1 + x2 + x3 + x4 + x5;', 'x2^2 + x1 + x2 + x3 + x4 + x5;', 'x3^2 + x1 + x2 + x3 + x4 + x5;', 'x4^2 + x1 + x2 + x3 + x4 + x5;']
-print performEndGame(l)
+#l = ['x1^2 - x1 + x2 + x3 + x4 + x5;', 'x2^2 + x1 + x2 + x3 + x4 + x5;', 'x3^2 + x1 + x2 + x3 + x4 + x5;', 'x4^2 + x1 + x2 + x3 + x4 + x5;']
+v=[2,1,1]
+print performEndGame(l,v,'x1')
